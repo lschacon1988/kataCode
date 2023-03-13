9e3a88b9bac067702905cfefef8bbb33d68163e8
lschacon1988 <lschacon1988@gmail.com>
2023-03-13 17:19:16 -0500

    Update bonApetit.py

--- bonApetit.py
+++ bonApetit.py
@@ -1,14 +1,14 @@
 def bonAppetit(bill, k, b):
     # Write your code here
-    total__anna = (sum(bill) - bill[k]) / 2
+    total__anna = int((sum(bill) - bill[k]) / 2)
     
     if (total__anna == b): print('Bon Appetit')
     
-    else: print(abs(int(total__anna) - b))
+    else: print(abs(total__anna - b))
     
     
 bill = [3 ,10 ,2 ,9]
 k=1
-b=7
+b=8
 
 bonAppetit(k=k,b=b,bill=bill) 
