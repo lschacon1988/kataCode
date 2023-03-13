9cb9eca641e27ef5908d30bc66bd12de558d7d99
lschacon1988 <lschacon1988@gmail.com>
2023-03-13 11:05:45 -0500

    se agregaron enunciados

--- migratory_birds.py
+++ migratory_birds.py
@@ -1,3 +1,13 @@
+'''Dada una serie de avistamientos de aves donde cada elemento
+representa una identificación de tipo de ave, determine la identificación
+del tipo avistado con más frecuencia. Si se ha detectado más de 1 tipo
+de esa cantidad máxima, devuelva la identificación más pequeña.
+
+Ejemplo
+arr:[1,1,2,2,3]
+
+Hay dos de cada uno de los tipos 1 y 2, y un avistamiento de tipo 3. Elija el menor de los dos tipos vistos dos veces: tipo 1.'''
+
 def migratoryBirds(arr:list):
     # Write your code here
     avistamientos= {key:arr.count(key) for key in arr  }
