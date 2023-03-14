3a99dd9604419dd1171ac3f65084f18e0a28a192
lschacon1988 <lschacon1988@gmail.com>
2023-03-14 09:07:31 -0500

    Update bonApetit.py

--- bonApetit.py
+++ bonApetit.py
@@ -12,3 +12,20 @@ k=1
 b=8
 
 bonAppetit(k=k,b=b,bill=bill) 
+
+def fix(l:list):
+    if(len(l)%2==0):
+        new_list=[]
+        for itm in l:
+            for ele in itm:
+                new_list.append(ele)
+    else:
+        new_list = [ele for ele in l for ele in ele]
+    def my_fun(x):
+        return x        
+    new_list.sort(reverse=True, key = my_fun)
+        
+    return new_list 
+
+l=[[3,4],[2,6]]
+print(fix(l))
